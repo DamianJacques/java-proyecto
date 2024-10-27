600f6c008d32b0752a40aeb7ca84e6d482a357cb
DamianJacques <damianjacques.79@gmail.com>
2024-10-27 12:55:59 -0300

    initial commit to spring

---
+++ javaProyecto/src/main/java/com/coderhouse/JavaProyectoApplication.java
@@ -0,0 +1,13 @@
+package com.coderhouse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaProyectoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaProyectoApplication.class, args);
+	}
+
+}
